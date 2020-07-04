6adde41276a656539c37f4890539f736dd61b5f2
James P. Galovic <jpgalovic@gmail.com>
2020-07-05 02:20:59 +1000

    Add: Wireframe Mode (Basic line toggle in/out)

--- Sandbox/Sandbox.cpp
+++ Sandbox/Sandbox.cpp
@@ -113,6 +113,8 @@ int main(int argc, char** argv)
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // Enter Wireframe Mode
+
 	// Gameloop
 	while (!myWindow.getShouldClose())
 	{
