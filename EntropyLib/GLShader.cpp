1aef144f5cd7dc2e507819ff4afd43f2831c7320
James P. Galovic <jpgalovic@gmail.com>
2020-07-05 15:38:07 +1000

    Add: unigorm location setting

--- EntropyLib/GLShader.cpp
+++ EntropyLib/GLShader.cpp
@@ -7,107 +7,143 @@
 #include <sstream>
 #include <iostream>
 
-/**
- * Loads and Compiles a Shader Program using the defined code files.
- * @param vertexPath, path to vertex shader source code.
- * @param fragmentPath, path to fragment shader source code.
- */
-Entropy::GLShader::GLShader(const char* vertexPath, const char* fragmentPath)
+namespace Entropy
 {
-	// Compile Shaders
-	unsigned int vertexShader = compile(vertexPath, GL_VERTEX_SHADER);
-	unsigned int fragmentShader = compile(fragmentPath, GL_FRAGMENT_SHADER);
-	// Create Program
-	ID = glCreateProgram();
-	glAttachShader(ID, vertexShader);
-	glAttachShader(ID, fragmentShader);
-	glLinkProgram(ID);
-	int success;
-	glGetProgramiv(ID, GL_LINK_STATUS, &success);
-	if (!success)
+	/**
+	 * Loads and Compiles a Shader Program using the defined code files.
+	 * @param vertexPath, path to vertex shader source code.
+	 * @param fragmentPath, path to fragment shader source code.
+	 */
+	GLShader::GLShader(const char* vertexPath, const char* fragmentPath)
 	{
-		char infolog[1024];
-		glGetProgramInfoLog(ID, 1024, NULL, infolog);
-		std::cout << "ERROR::PROGRAM::LINK_FAILED\n" << infolog << std::endl;
-	}
-	// Cleanup Memory
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-}
+		// Compile Shaders
+		unsigned int vertexShader = compile(vertexPath, GL_VERTEX_SHADER);
+		unsigned int fragmentShader = compile(fragmentPath, GL_FRAGMENT_SHADER);
 
-Entropy::GLShader::~GLShader()
-{
-	glDeleteProgram(ID);
-}
+		// Create Program
+		ID = glCreateProgram();
+		glAttachShader(ID, vertexShader);
+		glAttachShader(ID, fragmentShader);
+		glLinkProgram(ID);
 
-void Entropy::GLShader::use()
-{
-	glUseProgram(ID);
-}
-
-/**
- * Compiles a given shader unit.
- * @param path, path to shader source code.
- * @param type, type of shader been compiled.
- */
-unsigned int Entropy::GLShader::compile(const char* path, unsigned int type)
-{
-	std::ifstream file;
-	file.exceptions(std::ifstream::failbit || std::ifstream::badbit);
-	try
-	{
-		file.open(path);
-		std::stringstream stream;
-		stream << file.rdbuf();
-		file.close();
-		std::string source = stream.str();
-		const char* sourceCode = source.c_str();
-		unsigned int compileID = glCreateShader(type);
-		glShaderSource(compileID, 1, &sourceCode, NULL);
 		int success;
-		glGetShaderiv(compileID, GL_COMPILE_STATUS, &success);
+		char infoLog[1024];
+		glGetProgramiv(ID, GL_LINK_STATUS, &success);
 		if (!success)
 		{
-			char infolog[1024];
-			glGetShaderInfoLog(compileID, 1024, NULL, infolog);
-			std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infolog << std::endl;
+			glGetProgramInfoLog(ID, 1024, NULL, infoLog);
+			std::cout << "ERROR::PROGRAM_LINKING_ERROR:\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
 		}
-		return compileID;
+
+		// Cleanup Memory
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
 	}
-	catch (std::ifstream::failure e)
+
+	GLShader::~GLShader()
 	{
-		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+		glDeleteProgram(ID);
+	}
+
+	void GLShader::use()
+	{
+		glUseProgram(ID);
+	}
+
+	void GLShader::set1Bool(const char* name, bool v0) const
+	{
+		glUniform1i(glGetUniformLocation(ID, name), (int)v0);
+	}
+
+	void GLShader::set2Bool(const char* name, bool v0, bool v1) const
+	{
+		glUniform2i(glGetUniformLocation(ID, name), (int)v0, (int)v1);
+	}
+
+	void GLShader::set3Bool(const char* name, bool v0, bool v1, bool v2) const
+	{
+		glUniform3i(glGetUniformLocation(ID, name), (int)v0, (int)v1, (int)v2);
+	}
+
+	void GLShader::set4Bool(const char* name, bool v0, bool v1, bool v2, bool v3) const
+	{
+		glUniform4i(glGetUniformLocation(ID, name), v0, v1, v2, v3);
+	}
+
+	void GLShader::set1Int(const char* name, int v0) const
+	{
+		glUniform1i(glGetUniformLocation(ID, name), v0);
+	}
+
+	void GLShader::set2Int(const char* name, int v0, int v1) const
+	{
+		glUniform2i(glGetUniformLocation(ID, name), v0, v1);
+	}
+
+	void GLShader::set3Int(const char* name, int v0, int v1, int v2) const
+	{
+		glUniform3i(glGetUniformLocation(ID, name), v0, v1, v2);
+	}
+
+	void GLShader::set4Int(const char* name, int v0, int v1, int v2, int v3) const
+	{
+		glUniform4i(glGetUniformLocation(ID, name), v0, v1, v2, v3);
+	}
+
+	void GLShader::set1Float(const char* name, float v0) const
+	{
+		glUniform1f(glGetUniformLocation(ID, name), v0);
+	}
+
+	void GLShader::set2Float(const char* name, float v0, float v1) const
+	{
+		glUniform2f(glGetUniformLocation(ID, name), v0, v1);
+	}
+
+	void GLShader::set3Float(const char* name, float v0, float v1, float v2) const
+	{
+		glUniform3f(glGetUniformLocation(ID, name), v0, v1, v2);
+	}
+
+	void GLShader::set4Float(const char* name, float v0, float v1, float v2, float v3) const
+	{
+		glUniform4f(glGetUniformLocation(ID, name), v0, v1, v2, v3);
+	}
+
+	/**
+	 * Compiles a given shader unit.
+	 * @param path, path to shader source code.
+	 * @param type, type of shader been compiled.
+	 */
+	unsigned int GLShader::compile(const char* path, unsigned int type)
+	{
+		std::ifstream file;
+		file.exceptions(std::ifstream::failbit || std::ifstream::badbit);
+		try
+		{
+			file.open(path);
+			std::stringstream stream;
+			stream << file.rdbuf();
+			file.close();
+			std::string source = stream.str();
+			const char* sourceCode = source.c_str();
+			unsigned int compileID = glCreateShader(type);
+			glShaderSource(compileID, 1, &sourceCode, NULL);
+
+			int success;
+			char infoLog[1024];
+			glGetShaderiv(compileID, GL_COMPILE_STATUS, &success);
+			if (!success)
+			{
+				glGetShaderInfoLog(compileID, 1024, NULL, infoLog);
+				std::cout << "ERROR::SHADER_COMPILATION_ERROR:\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+			}
+
+			return compileID;
+		}
+		catch (std::ifstream::failure e)
+		{
+			std::cout << "ERROR::SHADER_FILE_NOT_SUCCESFULLY_READ" << std::endl;
+		}
 	}
 }
-
-/*
-
-void MyShader::setBool(const GLchar* name, bool value) const
-{
-	glUniform1i(glGetUniformLocation(ID, name), (int)value);
-}
-
-void MyShader::setInt(const GLchar* name, int value) const
-{
-	glUniform1i(glGetUniformLocation(ID, name), value);
-}
-
-void MyShader::setFloat(const GLchar* name, float value) const
-{
-	glUniform1f(glGetUniformLocation(ID, name), value);
-}
-
-void MyShader::setFloat2(const GLchar* name, float v0, float v1)
-{
-	glUniform2f(glGetUniformLocation(ID, name), v0, v1);
-}
-
-void MyShader::setFloat3(const GLchar* name, float v0, float v1, float v2)
-{
-	glUniform3f(glGetUniformLocation(ID, name), v0, v1, v2);
-}
-
-void MyShader::setFloat4(const GLchar* name, float v0, float v1, float v2, float v3)
-{
-	glUniform4f(glGetUniformLocation(ID, name), v0, v1, v2, v3);
-} */
--- EntropyLib/GLShader.h
+++ EntropyLib/GLShader.h
@@ -12,6 +12,21 @@ namespace Entropy
 
 		void use();
 
+		void set1Bool(const char* name, bool v0) const;
+		void set2Bool(const char* name, bool v0, bool v1) const;
+		void set3Bool(const char* name, bool v0, bool v1, bool v2) const;
+		void set4Bool(const char* name, bool v0, bool v1, bool v2, bool v3) const;
+
+		void set1Int(const char* name, int v0) const;
+		void set2Int(const char* name, int v0, int v1) const;
+		void set3Int(const char* name, int v0, int v1, int v2) const;
+		void set4Int(const char* name, int v0, int v1, int v2, int v3) const;
+
+		void set1Float(const char* name, float v0) const;
+		void set2Float(const char* name, float v0, float v1) const;
+		void set3Float(const char* name, float v0, float v1, float v2) const;
+		void set4Float(const char* name, float v0, float v1, float v2, float v3) const;
+
 	private:
 		unsigned int compile(const char* path, unsigned int type);
 	};
