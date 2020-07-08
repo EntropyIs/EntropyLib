916ff7bf03996232be264415e931baa79fcad37b
James P. Galovic <jpgalovic@gmail.com>
2020-07-09 01:22:22 +1000

    Add: Addition, subtraction and multiplication of vectors

--- EntropyLib/Matrix3.cpp
+++ EntropyLib/Matrix3.cpp
@@ -8,3 +8,36 @@ Entropy::Math::Vector3 Entropy::Math::operator*(const Matrix3& left, const Vecto
 		left.r2c0 * right.i + left.r2c1 * right.j + left.r2c2 * right.k
 	);
 }
+
+Entropy::Math::Matrix3 Entropy::Math::operator*(const Matrix3& left, const Matrix3& right)
+{
+	return Matrix3(
+		left.r0c0 * right.r0c0 + left.r0c1 * right.r1c0 + left.r0c2 * right.r2c0,
+		left.r0c0 * right.r0c1 + left.r0c1 * right.r1c1 + left.r0c2 * right.r2c1,
+		left.r0c0 * right.r0c2 + left.r0c1 * right.r1c2 + left.r0c2 * right.r2c2,
+		left.r1c0 * right.r0c0 + left.r1c1 * right.r1c0 + left.r1c2 * right.r2c0,
+		left.r1c0 * right.r0c1 + left.r1c1 * right.r1c1 + left.r1c2 * right.r2c1,
+		left.r1c0 * right.r0c2 + left.r1c1 * right.r1c2 + left.r1c2 * right.r2c2,
+		left.r2c0 * right.r0c0 + left.r2c1 * right.r1c0 + left.r2c2 * right.r2c0,
+		left.r2c0 * right.r0c1 + left.r2c1 * right.r1c1 + left.r2c2 * right.r2c1,
+		left.r2c0 * right.r0c2 + left.r2c1 * right.r1c2 + left.r2c2 * right.r2c2
+	);
+}
+
+Entropy::Math::Matrix3 Entropy::Math::operator+(const Matrix3& left, const Matrix3& right)
+{
+	return Matrix3(
+		left.r0c0 + right.r0c0, left.r0c1 + right.r0c1, left.r0c2 + right.r0c2,
+		left.r1c0 + right.r1c0, left.r1c1 + right.r1c1, left.r1c2 + right.r1c2,
+		left.r2c0 + right.r2c0, left.r2c1 + right.r2c1, left.r2c2 + right.r2c2
+	);
+}
+
+Entropy::Math::Matrix3 Entropy::Math::operator-(const Matrix3& left, const Matrix3& right)
+{
+	return Matrix3(
+		left.r0c0 - right.r0c0, left.r0c1 - right.r0c1, left.r0c2 - right.r0c2,
+		left.r1c0 - right.r1c0, left.r1c1 - right.r1c1, left.r1c2 - right.r1c2,
+		left.r2c0 - right.r2c0, left.r2c1 - right.r2c1, left.r2c2 - right.r2c2
+	);
+}
--- EntropyLib/Matrix3.h
+++ EntropyLib/Matrix3.h
@@ -11,14 +11,18 @@ namespace Entropy
 			float r2c0; float r2c1; float r2c2;
 
 			Matrix3(
-				float r0c0=1, float r0c1 =0, float r0c2 =0,
-				float r1c0=0, float r1c1 =1, float r1c2 =0,
-				float r2c0=0, float r2c1 =0, float r2c2 =1) :
+				float r0c0 = 1, float r0c1 = 0, float r0c2 = 0,
+				float r1c0 = 0, float r1c1 = 1, float r1c2 = 0,
+				float r2c0 = 0, float r2c1 = 0, float r2c2 = 1) :
 				r0c0(r0c0), r0c1(r0c1), r0c2(r0c2),
-				r1c0(r1c0), r1c1(r1c1), r1c2(r1c2), 
-				r2c0(r2c0), r2c1(r2c1), r2c2(r2c2){}
+				r1c0(r1c0), r1c1(r1c1), r1c2(r1c2),
+				r2c0(r2c0), r2c1(r2c1), r2c2(r2c2) {}
 		};
 		Vector3 __declspec(dllexport) operator* (const Matrix3& left, const Vector3& right);
+
+		Matrix3 __declspec(dllexport) operator* (const Matrix3& left, const Matrix3& right);
+		Matrix3 __declspec(dllexport) operator+ (const Matrix3& left, const Matrix3& right);
+		Matrix3 __declspec(dllexport) operator- (const Matrix3& left, const Matrix3& right);
 	}
 }
 
