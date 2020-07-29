4a2acc8482f0eee1726817a7ba2818bb4bd22a30
James P. Galovic <jpgalovic@gmail.com>
2020-07-29 11:47:32 +1000

    Inital add of asteroids project

---
+++ Asteroids/Source.cpp
@@ -0,0 +1,15 @@
+#include <Entropy/Graphics/Window.h>
+#include <Entropy/Graphics/Shader.h>
+
+using namespace Entropy;
+
+int main(int argc, char* argv[])
+{
+	// Load OpenGL Window
+	Graphics::Window window("Asteroids Example Project.", 800, 600);
+	window.captureMouse();
+
+	// Load Shaders
+	Graphics::Shader shader("vBaseShader.glsl", "fLighting.glsl");
+	Graphics::Shader skyboxShader("vCubeMap.glsl", "fCubeMap.glsl");
+}
