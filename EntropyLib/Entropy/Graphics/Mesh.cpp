634c9174d33c0b3c12ad729d9c5a954031872a6e
James P. Galovic <jpgalovic@gmail.com>
2020-08-01 15:08:03 +1000

    Add: Quad mesh

--- EntropyLib/Entropy/Graphics/Mesh.cpp
+++ EntropyLib/Entropy/Graphics/Mesh.cpp
@@ -204,3 +204,42 @@ void Entropy::Graphics::SkyboxMesh::Draw(Shader& shader, Math::Mat4& view, Math:
 	glBindVertexArray(0);
 	glDepthFunc(GL_LESS);
 }
+
+Entropy::Graphics::QuadMesh::QuadMesh()
+{
+	float Verts[] =
+	{
+		 1.0f,  1.0f,  1.0f, 1.0f,
+		 1.0f, -1.0f,  1.0f, 0.0f,
+		-1.0f,  1.0f,  0.0f, 1.0f,
+		-1.0f, -1.0f,  0.0f, 0.0f
+	};
+	unsigned int Inds[] =
+	{
+		0,1,2,
+		1,2,3
+	};
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Verts), &Verts, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Inds), &Inds, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+	glBindVertexArray(0);
+}
+
+void Entropy::Graphics::QuadMesh::Draw(Shader& shader, FrameBuffer& frameBuffer)
+{
+	shader.use();
+	glBindVertexArray(VAO);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, frameBuffer.ColorBuffer);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+}
